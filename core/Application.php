ba48e46cfd72a722dda5525f91af44f33ab9e168
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-07-25 13:34:23 +0545

    Connect to database

--- core/Application.php
+++ core/Application.php
@@ -10,13 +10,16 @@ class Application {
     public Response $response;
     public static Application $app;
     public Controller $controller;
+    public Database $db;
 
-    public function __construct($rootPath){
+    public function __construct($rootPath, array $config){
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
         $this->response = new Response();
         $this->request = new Request();
         $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database($config['db']);
     }
 
     public function run(){
